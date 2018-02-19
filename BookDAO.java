fefd1e30eeca165259df0ec334c2b684f6134ff3
adamlup <adamlupi@gmail.com>
2018-02-19 12:54:51 +0100

    add methods operating on database

--- BookDAO.java
+++ BookDAO.java
@@ -6,52 +6,150 @@ class BookDAO{
     
     public static void addBook(Statement stmt, String ISBN, String author, String title, String publisher, String publicationYear, String price, String type){
         try {
-            // Scanner sc = new Scanner(System.in);
-            // System.out.print("\nEnter a ISBN: ");
-            // int ISBN = sc.nextInt();
-
-            // System.out.print("\nEnter a author: ");
-            // String author = sc.nextLine();
-    
-            // System.out.print("\nEnter a title: ");
-            // String title = sc.nextLine();
-    
-            // System.out.print("\nEnter a publisher: ");
-            // String publisher = sc.nextLine();
-
-            // System.out.print("\nEnter a publication year: ");
-            // int publicationYear = sc.nextInt();
-
-            // System.out.print("\nEnter a price: ");
-            // int price = sc.nextInt();
-
-            // System.out.print("\nEnter a type: ");
-            // int type = sc.nextInt();
-
             String query =("insert into Books values(ISBN, author, title, publisher, publicationYear, price, type");
             stmt.executeUpdate(query);
-            System.out.println("aaaa");
-            // ResultSet rs = stmt.executeQuery("SELECT * FROM Books");
-            // System.out.println("bbbb");
-            // while(rs.next()){
-            //     int ISBN  = rs.getInt("ISBN");
-            //     String author = rs.getString("author");
-            //     String title = rs.getString("title");
-            //     String publisher = rs.getString("publisher");
-            //     int publicationYear = rs.getString("publication_year");
-            //     int price = rs.getInt("price");
-            //     int type = rs.getInt("type");
-
-            //     //Display values
-            //     System.out.println("ID: " + id + '\n');
-            //     System.out.println("first_name: " + firstName + '\n');
-            //     System.out.println("last_name: " + lastName+ '\n');
-            //     System.out.println("phone_number: " + phoneNumber+ '\n');
-            //     System.out.println("email: " + email+ '\n');
-            //     System.out.println("application_code: " + applcationCode+ '\n');
-            // }
-        //SQLiteJDBC.closeConnection(rs, stmt);
         } catch ( Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void editBook(Statement stmt, String givenTitle){
+        try{
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Books Where title like '%" + givenTitle + "%'");
+            while ( rs.next() ) {
+                String ISBN = rs.getString("ISBN");
+                String author = rs.getString("author");
+                String title  = rs.getString("title");
+                String publisher = rs.getString("publisher");
+                String publicationYear = rs.getString("publication_year");
+                String price = rs.getString("price");
+                String type = rs.getString("type");
+                
+                System.out.println( "ISBN = " + ISBN );
+                System.out.println( "author = " + author );
+                System.out.println( "title = " + title );
+                System.out.println( "publisher = " + publisher );
+                System.out.println( "publicationYear = " + publicationYear );
+                System.out.println( "price = " + price );
+                System.out.println( "type = " + type );
+                System.out.println();
+             }
+        }catch ( Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void deleteBook(Statement stmt, String givenTitle){
+        try{
+            String query = ("DELETE from Books where title like '%" + givenTitle + "%'");
+            stmt.executeUpdate(query);
+            
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM Books;" );
+            
+            while ( rs.next() ) {
+                String ISBN = rs.getString("ISBN");
+                String author = rs.getString("author");
+                String title  = rs.getString("title");
+                String publisher = rs.getString("publisher");
+                String publicationYear = rs.getString("publication_year");
+                String price = rs.getString("price");
+                String type = rs.getString("type");
+                
+                System.out.println( "ISBN = " + ISBN );
+                System.out.println( "author = " + author );
+                System.out.println( "title = " + title );
+                System.out.println( "publisher = " + publisher );
+                System.out.println( "publicationYear = " + publicationYear );
+                System.out.println( "price = " + price );
+                System.out.println( "type = " + type );
+                System.out.println();
+             }
+        }catch( Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void booksByAuthor(Statement stmt, String givenAuthor){
+        try{
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Books Where author like '%" + givenAuthor + "%'");
+            while ( rs.next() ) {
+                String ISBN = rs.getString("ISBN");
+                String author = rs.getString("author");
+                String title  = rs.getString("title");
+                String publisher = rs.getString("publisher");
+                String publicationYear = rs.getString("publication_year");
+                String price = rs.getString("price");
+                String type = rs.getString("type");
+                
+                System.out.println( "ISBN = " + ISBN );
+                System.out.println( "author = " + author );
+                System.out.println( "title = " + title );
+                System.out.println( "publisher = " + publisher );
+                System.out.println( "publicationYear = " + publicationYear );
+                System.out.println( "price = " + price );
+                System.out.println( "type = " + type );
+                System.out.println();
+             }
+        }catch ( Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void searchBooks(Statement stmt, String searchPhrase){
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Books where (ISBN || author || title || publisher || publication_year || price || type) like '%" + searchPhrase + "%'");
+            while(rs.next()){
+                    
+                String ISBN = rs.getString("ISBN");
+                String author = rs.getString("author");
+                String title  = rs.getString("title");
+                String publisher = rs.getString("publisher");
+                String publicationYear = rs.getString("publication_year");
+                String price = rs.getString("price");
+                String type = rs.getString("type");
+                
+                System.out.println( "ISBN = " + ISBN );
+                System.out.println( "author = " + author );
+                System.out.println( "title = " + title );
+                System.out.println( "publisher = " + publisher );
+                System.out.println( "publicationYear = " + publicationYear );
+                System.out.println( "price = " + price );
+                System.out.println( "type = " + type );
+                System.out.println();
+                }
+        } catch (Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void sortedBooks(Statement stmt){
+        try{
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Books ORDER BY title ASC");
+            while(rs.next()){
+                    
+                String ISBN = rs.getString("ISBN");
+                String author = rs.getString("author");
+                String title  = rs.getString("title");
+                String publisher = rs.getString("publisher");
+                String publicationYear = rs.getString("publication_year");
+                String price = rs.getString("price");
+                String type = rs.getString("type");
+                
+                System.out.println( "ISBN = " + ISBN );
+                System.out.println( "author = " + author );
+                System.out.println( "title = " + title );
+                System.out.println( "publisher = " + publisher );
+                System.out.println( "publicationYear = " + publicationYear );
+                System.out.println( "price = " + price );
+                System.out.println( "type = " + type );
+                System.out.println();
+            }
+        } catch (Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
