a88314270d832fde279df80d97271b7c8ba2f3ef
adamlup <adamlupi@gmail.com>
2018-02-19 11:05:20 +0100

    add addBook

--- Controller.java
+++ Controller.java
@@ -1,3 +1,34 @@
+import java.sql.Statement;
+import java.util.Scanner;
+
 class Controller{
-    
+    private View view;
+
+    // Controller(View view){
+    //     this.view = view;
+    // }
+
+    void Start(){
+        Statement stmt = DatabaseDAO.connection();
+        System.out.println("aaaaa");
+
+        addNewBook(stmt);
+    }
+
+    private void addNewBook(Statement stmt){
+
+        String ISBN = view.getUserInput();
+        String author = view.getUserInput();
+        String title = view.getUserInput();
+        String publisher = view.getUserInput();
+        String publicationYear = view.getUserInput();
+        String price = view.getUserInput();
+        String type = view.getUserInput();
+        BookDAO.addBook(stmt, ISBN, author, title, publisher, publicationYear, price, type);
+
+    }
+
+    void setISBN(){
+        view.getUserInput();
+    }
 }
