689ad9db0ee9d7fd5e1bf565a328d5762826ff96
adamlup <adamlupi@gmail.com>
2018-02-19 11:03:57 +0100

    add connectrion to database

--- DatabaseDAO.java
+++ DatabaseDAO.java
@@ -1,3 +1,22 @@
+import java.sql.*;
+
 class DatabaseDAO{
     
+    private static Statement stmt = null;
+
+    public static Statement connection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:db.sql");
+            stmt = c.createStatement();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+
+        System.out.println("Opened database successfully");
+        return stmt;
+    }
+
 }
